93697d1f5cc9ea6356a88f5f15a9afbef7d203a0
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-08-03 22:23:09 +0700

    implement repository oauth

---
+++ internal/oauth/repository/oauth_client_repository.go
@@ -0,0 +1,32 @@
+package repository
+
+import (
+	"go_online_course_v2/internal/oauth/entity"
+	"go_online_course_v2/pkg/response"
+	"gorm.io/gorm"
+)
+
+type OauthClientRepository interface {
+	FindByClientIDAndClientSecret(clientID string, clientSecret string) (*entity.OauthClient, *response.Errors)
+}
+
+type oauthClientRepository struct {
+	db *gorm.DB
+}
+
+// FindByClientIDAndClientSecret implements OauthClientRepository
+func (repository *oauthClientRepository) FindByClientIDAndClientSecret(clientID string, clientSecret string) (*entity.OauthClient, *response.Errors) {
+	var oauthClient entity.OauthClient
+
+	if err := repository.db.Where("client_id = ?", clientID).Where("client_secret = ?", clientSecret).Error; err != nil {
+		return nil, &response.Errors{
+			Code: 500,
+			Err:  err,
+		}
+	}
+	return &oauthClient, nil
+}
+
+func NewOauthClientRepository(db *gorm.DB) OauthClientRepository {
+	return &oauthClientRepository{db: db}
+}
