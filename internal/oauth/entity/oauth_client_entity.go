8d1376fd6c3f67f6b7e86360643ac2b805768eb6
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-08-03 11:27:27 +0700

    implement oauth entity

---
+++ internal/oauth/entity/oauth_client_entity.go
@@ -0,0 +1,18 @@
+package entity
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type OauthClient struct {
+	ID           int64          `json:"id"`
+	ClientID     string         `json:"client_id"`
+	ClientSecret string         `json:"client_secret"`
+	Name         string         `json:"name"`
+	Redirect     string         `json:"redirect"`
+	Scope        string         `json:"scope"`
+	CreatedAt    *time.Time     `json:"created_at"`
+	UpdatedAt    *time.Time     `json:"updated_at"`
+	DeletedAt    gorm.DeletedAt `json:"deleted_at"`
+}
---
+++ internal/oauth/entity/oauth_refresh_token_entity.go
@@ -0,0 +1,18 @@
+package entity
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type OauthRefreshToken struct {
+	ID                 int64             `json:"id"`
+	OauthAccessToken   *OauthAccessToken `gorm:"foreignKey:OauthAccessTokenID;references:ID"`
+	OauthAccessTokenID *int64            `json:"oauth_access_token_id"`
+	UserID             int64             `json:"user_id"`
+	Token              string            `json:"token"`
+	ExpiredAt          *time.Time        `json:"expired_at"`
+	CreatedAt          *time.Time        `json:"created_at"`
+	UpdatedAt          *time.Time        `json:"updated_at"`
+	DeletedAt          gorm.DeletedAt    `json:"deleted_at"`
+}
