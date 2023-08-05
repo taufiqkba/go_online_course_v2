74776579e27e0744b21fe67c29bc2ea2fe6a73d8
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-08-05 12:36:38 +0700

    add severals libraries and setup main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"go_online_course_v2/internal/oauth/delivery"
+	repository2 "go_online_course_v2/internal/oauth/repository"
+	usecase3 "go_online_course_v2/internal/oauth/usecase"
 	"go_online_course_v2/internal/register/delivery/http"
 	usecase2 "go_online_course_v2/internal/register/usecase"
 	"go_online_course_v2/internal/user/repository"
@@ -18,6 +21,19 @@ func main() {
 
 	registerUseCase := usecase2.NewRegisterUseCase(userUseCase)
 	http.NewRegisterHandler(registerUseCase).Route(&r.RouterGroup)
+
+	oauthClientRepository := repository2.NewOauthClientRepository(db)
+	oauthAccessTokenRepository := repository2.NewOauthAccessTokenRepository(db)
+	oauthRefreshTokenRepository := repository2.NewOauthRefreshTokenRepository(db)
+	oauthUseCase := usecase3.NewOauthUseCase(
+		oauthClientRepository,
+		oauthAccessTokenRepository,
+		oauthRefreshTokenRepository,
+		userUseCase,
+	)
+
+	delivery.NewOauthHandler(oauthUseCase).Route(&r.RouterGroup)
+
 	err := r.Run()
 	if err != nil {
 		return
