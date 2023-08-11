807cf828be2426f6b5ba7cedcf54b8c95be91f86
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-08-11 23:45:49 +0700

    implement delivery admin

---
+++ internal/admin/delivery/http/admin_handler.go
@@ -0,0 +1,88 @@
+package http
+
+import (
+	"github.com/gin-gonic/gin"
+	"go_online_course_v2/internal/admin/dto"
+	"go_online_course_v2/internal/admin/usecase"
+	"go_online_course_v2/pkg/response"
+	"net/http"
+	"strconv"
+)
+
+type AdminHandler struct {
+	useCase usecase.AdminUseCase
+}
+
+func NewAdminHandler(useCase usecase.AdminUseCase) *AdminHandler {
+	return &AdminHandler{useCase: useCase}
+}
+
+func (handler *AdminHandler) Create(ctx *gin.Context) {
+	var input dto.AdminRequestBody
+
+	//response error
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, response.Response(
+			http.StatusBadRequest,
+			http.StatusText(http.StatusBadRequest),
+			err.Error(),
+		))
+		ctx.Abort()
+		return
+	}
+
+	//	create data
+	_, err := handler.useCase.Create(input)
+	if err != nil {
+		ctx.JSON(int(err.Code), response.Response(
+			int(err.Code),
+			http.StatusText(int(err.Code)),
+			err.Err.Error(),
+		))
+		ctx.Abort()
+		return
+	}
+
+	//response success created
+	ctx.JSON(http.StatusCreated, response.Response(
+		http.StatusCreated,
+		http.StatusText(http.StatusCreated),
+		"Created success",
+	))
+}
+
+func (handler *AdminHandler) Update(ctx *gin.Context) {
+	id, _ := strconv.Atoi(ctx.Param("id"))
+	var input dto.AdminRequestBody
+
+	//response error
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, response.Response(
+			http.StatusBadRequest,
+			http.StatusText(http.StatusBadRequest),
+			err.Error(),
+		))
+		ctx.Abort()
+		return
+	}
+
+	//	update data
+	data, err := handler.useCase.Update(id, input)
+	if err != nil {
+		ctx.JSON(int(err.Code), response.Response(
+			int(err.Code),
+			http.StatusText(int(err.Code)),
+			err.Err.Error(),
+		))
+		ctx.Abort()
+		return
+	}
+
+	//response success
+	ctx.JSON(http.StatusOK, response.Response(
+		http.StatusOK,
+		http.StatusText(http.StatusOK),
+		data,
+	))
+
+}
