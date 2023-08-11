13aea7af5810b18f2e853e40ce4fe656b7737285
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-08-11 13:27:25 +0700

    entity admin

--- internal/admin/entity/admin_entity.go
+++ internal/admin/entity/admin_entity.go
@@ -1,4 +1,20 @@
 package entity
 
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
 type Admin struct {
+	ID          int64          `json:"id"`
+	Name        string         `json:"name"`
+	Email       string         `json:"email"`
+	Password    string         `json:"password"`
+	CreatedByID *int64         `json:"created_by" gorm:"column:created_by"`
+	CreatedBy   *Admin         `json:"-" gorm:"foreignKey:CreatedByID;references:ID"`
+	UpdatedByID *int64         `json:"updated_by" gorm:"updated_by"`
+	UpdatedBy   *Admin         `json:"-" gorm:"foreignKey:UpdatedByID;references:ID"`
+	CreatedAt   *time.Time     `json:"created_at"`
+	UpdatedAt   *time.Time     `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `json:"deleted_at"`
 }
