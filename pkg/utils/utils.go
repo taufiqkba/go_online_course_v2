8daee38ed5a717f90792f33a74ad6b0c21d31aa4
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-08-11 13:38:41 +0700

    create pagination func

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,6 +1,9 @@
 package utils
 
-import "math/rand"
+import (
+	"gorm.io/gorm"
+	"math/rand"
+)
 
 func RandString(length int) string {
 	var letterRune = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
@@ -20,4 +23,27 @@ func RandNumber(length int) string {
 		b[i] = letterRune[rand.Intn(len(letterRune))]
 	}
 	return string(b)
+}
+
+func Paginate(offset int, limit int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		page := offset
+
+		//	if page value <= 0 default page 1
+		if page <= 0 {
+			page = 1
+		}
+
+		pageSize := limit
+		switch {
+		case pageSize > 100:
+			pageSize = 100
+
+		case pageSize <= 0:
+			pageSize = 10
+		}
+
+		offset := (page - 1) * limit
+		return db.Offset(offset).Limit(pageSize)
+	}
 }
