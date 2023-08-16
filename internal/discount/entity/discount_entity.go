10b826bf9af759f88cbb2b35a3d5f8016ae8a41f
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-08-16 15:57:55 +0700

    create discount entity

---
+++ internal/discount/entity/discount_entity.go
@@ -0,0 +1,26 @@
+package entity
+
+import (
+	"go_online_course_v2/internal/admin/entity"
+	"gorm.io/gorm"
+	"time"
+)
+
+type Discount struct {
+	ID                int64          `json:"id"`
+	Name              string         `json:"name"`
+	Code              string         `json:"code"`
+	Quantity          string         `json:"quantity"`
+	RemainingQuantity string         `json:"remaining_quantity"`
+	Type              string         `json:"type "`
+	Value             int64          `json:"value "`
+	StartDate         *time.Time     `json:"start_date"`
+	EndDate           *time.Time     `json:"end_date"`
+	CreatedByID       *int64         `json:"created_by" gorm:"column:created_by"`
+	CreatedBy         *entity.Admin  `json:"-" gorm:"foreignKey:CreatedByID;references:ID"`
+	UpdatedByID       *int64         `json:"updated_by" gorm:"column:updated_by"`
+	UpdatedBy         *entity.Admin  `json:"-" gorm:"foreignKey:UpdatedByID;references:ID"`
+	CreatedAt         *time.Time     `json:"created_at"`
+	UpdatedAt         *time.Time     `json:"updated_at"`
+	DeletedAt         gorm.DeletedAt `json:"deleted_at"`
+}
