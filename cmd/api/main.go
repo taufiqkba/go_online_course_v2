113c807622373cb0f5685c52ab010290327df936
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-08-16 10:26:32 +0700

    implement injector product category

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	injector4 "go_online_course_v2/internal/admin/injector"
 	injector3 "go_online_course_v2/internal/forgot_password/injector"
 	injector2 "go_online_course_v2/internal/oauth/injector"
+	injector5 "go_online_course_v2/internal/product_category/injector"
 	"go_online_course_v2/internal/register/injector"
 	"go_online_course_v2/pkg/db/mysql"
 )
@@ -17,6 +18,7 @@ func main() {
 	injector2.InitializedService(db).Route(&r.RouterGroup)
 	injector3.InitializedService(db).Route(&r.RouterGroup)
 	injector4.InitializedService(db).Route(&r.RouterGroup)
+	injector5.InitializedService(db).Route(&r.RouterGroup)
 	err := r.Run()
 	if err != nil {
 		return
