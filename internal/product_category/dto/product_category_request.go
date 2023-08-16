fa43d72281483eb5a77e0bf3cb88afdd30f9127b
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-08-16 09:43:23 +0700

    crate dto product category

---
+++ internal/product_category/dto/product_category_request.go
@@ -0,0 +1,10 @@
+package dto
+
+import "mime/multipart"
+
+type ProductCategoryRequestBody struct {
+	Name      string                `form:"name" binding:"required"`
+	Image     *multipart.FileHeader `form:"image"`
+	CreatedBy *int64
+	UpdatedBy *int64
+}
