379bd1ad5b51a79af3686dfb77947e00ebcc5e1a
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-08-16 12:25:03 +0700

    create dto product

---
+++ internal/product/dto/product_request.go
@@ -0,0 +1,15 @@
+package dto
+
+import "mime/multipart"
+
+type ProductRequestBody struct {
+	ProductCategoryID int64                 `form:"product_category_id" binding:"required"`
+	Title             string                `form:"title" binding:"required"`
+	Image             *multipart.FileHeader `form:"image"`
+	Video             *multipart.FileHeader `form:"video"`
+	Description       string                `form:"description:" binding:"required"`
+	IsHighlighted     bool                  `form:"is_highlighted"`
+	Price             int64                 `form:"price" binding:"required"`
+	CreatedBy         *int64
+	UpdatedBy         *int64
+}
