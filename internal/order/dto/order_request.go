2b46af800b8468baf552f04d6d989ada596be7c1
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-08-17 21:08:39 +0700

    Create order dto

---
+++ internal/order/dto/order_request.go
@@ -0,0 +1,9 @@
+package dto
+
+type OrderRequestBody struct {
+	DiscountCode *string `json:"discount_code"`
+	ProductID    *int64  `json:"product_id"`
+	UserID       int64   `json:"user_id"`
+	Email        string  `json:"email"`
+	Status       string  `json:"status"`
+}
