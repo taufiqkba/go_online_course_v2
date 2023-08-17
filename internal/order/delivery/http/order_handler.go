4fe805733928d95c4d6b5b3ed91799e49af3f425
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-08-17 22:06:43 +0700

    create order handler

---
+++ internal/order/delivery/http/order_handler.go
@@ -0,0 +1,93 @@
+package http
+
+import (
+	"github.com/gin-gonic/gin"
+	"go_online_course_v2/internal/order/dto"
+	"go_online_course_v2/internal/order/usecase"
+	"go_online_course_v2/pkg/response"
+	"go_online_course_v2/pkg/utils"
+	"net/http"
+	"strconv"
+)
+
+type OrderHandler struct {
+	useCase usecase.OrderUseCase
+}
+
+func NewOrderHandler(useCase usecase.OrderUseCase) *OrderHandler {
+	return &OrderHandler{useCase: useCase}
+}
+
+func (handler *OrderHandler) Route(r *gin.RouterGroup) {
+
+}
+
+func (handler *OrderHandler) FindAllByUserID(ctx *gin.Context) {
+	offset, _ := strconv.Atoi(ctx.Query("offset"))
+	limit, _ := strconv.Atoi(ctx.Query("limit"))
+	user := utils.GetCurrentUser(ctx)
+
+	data := handler.useCase.FindAllByUserID(int(user.ID), offset, limit)
+	ctx.JSON(http.StatusOK, response.Response(
+		http.StatusOK,
+		http.StatusText(http.StatusOK),
+		data,
+	))
+}
+
+func (handler *OrderHandler) FindByID(ctx *gin.Context) {
+	id, _ := strconv.Atoi(ctx.Param("id"))
+	//user := utils.GetCurrentUser(ctx)
+
+	data, err := handler.useCase.FindOneByID(id)
+	if err != nil {
+		ctx.JSON(int(err.Code), response.Response(
+			int(err.Code),
+			http.StatusText(int(err.Code)),
+			data,
+		))
+		ctx.Abort()
+		return
+	}
+
+	ctx.JSON(http.StatusOK, response.Response(
+		http.StatusOK,
+		http.StatusText(http.StatusOK),
+		data,
+	))
+}
+
+func (handler *OrderHandler) Create(ctx *gin.Context) {
+	var input dto.OrderRequestBody
+
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, response.Response(
+			http.StatusBadRequest,
+			http.StatusText(http.StatusBadRequest),
+			err,
+		))
+		ctx.Abort()
+		return
+	}
+	user := utils.GetCurrentUser(ctx)
+
+	input.UserID = user.ID
+	input.Email = user.Email
+
+	data, err := handler.useCase.Create(input)
+	if err != nil {
+		ctx.JSON(int(err.Code), response.Response(
+			int(err.Code),
+			http.StatusText(int(err.Code)),
+			data,
+		))
+		ctx.Abort()
+		return
+	}
+
+	ctx.JSON(http.StatusOK, response.Response(
+		http.StatusOK,
+		http.StatusText(http.StatusOK),
+		data,
+	))
+}
